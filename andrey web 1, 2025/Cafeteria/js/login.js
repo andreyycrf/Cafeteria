20232ddcc8e2e0854675170da92ecd6f413324e3
andreyycrf <andreyclaudinoass@gmail.com>
2025-09-15 17:15:00 -0300

    atualização

--- andrey web 1, 2025/Cafeteria/js/login.js
+++ andrey web 1, 2025/Cafeteria/js/login.js
@@ -3,7 +3,7 @@ function logar(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if(login == "admin" && senha == "admin"){
+    if(login == "admin" && senha == "admin"){ //login é admin e senha é admin
         alert('Sucesso!!');
         location.href = "cafeteria.html";
     }else{
